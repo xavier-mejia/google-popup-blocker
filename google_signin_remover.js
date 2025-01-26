5fde5bc14f8247ffdac89fd4c8621fbb3d997c70
Xavier Mejia <90576608+xavier-mejia@users.noreply.github.com>
2025-01-25 18:21:39 -0600

    add extra URL patterns for improving compatibility

--- google_signin_remover.js
+++ google_signin_remover.js
@@ -1,6 +1,13 @@
 // Blocking Google's popup script before the HTTP request is made.
 browser.webRequest.onBeforeRequest.addListener(
   () => ({ cancel: true }),
-  { urls: ['https://accounts.google.com/gsi/client'], types: ["script"] },
+  { 
+    urls: [
+      'https://accounts.google.com/gsi/client/*',
+      'https://accounts.google.com/gsi/client?*',
+      'https://accounts.google.com/gsi/client',
+      ],
+    types: ["script"]  
+  },
   ["blocking"],
 );
