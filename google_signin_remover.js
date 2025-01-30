fe3cd6572d96291a3a8c0ad9368d42dfc8d125d3
Xavier Mejia <90576608+xavier-mejia@users.noreply.github.com>
2025-01-29 20:29:58 -0600

    add LinkedIn's gsi script to be blocked

--- google_signin_remover.js
+++ google_signin_remover.js
@@ -6,6 +6,7 @@ browser.webRequest.onBeforeRequest.addListener(
       'https://accounts.google.com/gsi/client/*',
       'https://accounts.google.com/gsi/client?*',
       'https://accounts.google.com/gsi/client',
+      'https://static.licdn.com/aero-v1/sc/h/29rdkxlvag0d3cpj96fiilbju', // LinkedIn
       ],
     types: ["script"]  
   },
