dc701593b19ae69da519c763fcf8c3315d48d604
Freddie Sackur <freddie.git@dustyfox.uk>
2025-08-12 16:36:19 +0100

    add iframe url for linkedin

--- google_signin_remover.js
+++ google_signin_remover.js
@@ -1,14 +1,15 @@
 // Blocking Google's popup script before the HTTP request is made.
 browser.webRequest.onBeforeRequest.addListener(
   () => ({ cancel: true }),
-  { 
+  {
     urls: [
       'https://accounts.google.com/gsi/client/*',
       'https://accounts.google.com/gsi/client?*',
       'https://accounts.google.com/gsi/client',
       'https://static.licdn.com/aero-v1/sc/h/29rdkxlvag0d3cpj96fiilbju', // LinkedIn
+      'https://accounts.google.com/gsi/iframe/select?*',  // also LinkedIn
       ],
-    types: ["script"]  
+    types: ["script"]
   },
   ["blocking"],
 );
