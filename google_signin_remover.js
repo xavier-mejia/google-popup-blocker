ef8d335f7c2ee4312806794660fbd65cdee69d2d
Xavier Mejia <90576608+xavier-mejia@users.noreply.github.com>
2025-10-04 15:31:27 -0500

    organize URLs to be blocked, and add new LinkedIn URL

--- google_signin_remover.js
+++ google_signin_remover.js
@@ -1,14 +1,20 @@
+const LINKEDIN_URLS = [
+  'https://static.licdn.com/aero-v1/sc/h/29rdkxlvag0d3cpj96fiilbju',
+  'https://static.licdn.com/sc/h/aofke6z5sqc44bjlvj6yr05c8'
+];
+
+const GOOGLE_URLS = [
+  'https://accounts.google.com/gsi/client/*',
+  'https://accounts.google.com/gsi/client?*',
+  'https://accounts.google.com/gsi/client'
+];
+
 // Blocking Google's popup script before the HTTP request is made.
 browser.webRequest.onBeforeRequest.addListener(
   () => ({ cancel: true }),
-  { 
-    urls: [
-      'https://accounts.google.com/gsi/client/*',
-      'https://accounts.google.com/gsi/client?*',
-      'https://accounts.google.com/gsi/client',
-      'https://static.licdn.com/aero-v1/sc/h/29rdkxlvag0d3cpj96fiilbju', // LinkedIn
-      ],
-    types: ["script"]  
+  {
+    urls: [...GOOGLE_URLS, ...LINKEDIN_URLS],
+    types: ["script"]
   },
-  ["blocking"],
+  ["blocking"]
 );
